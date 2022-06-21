0f551ce46ed70c3243b9561924fe36bfad852918
Duc Dang <duc.dang@orientsoftware.com>
2022-06-21 10:27:17 +0700

    Improve code style

--- HackerRankSolvings/HackerRankSolvings/Challenges/WarmUp/RepeatedString.cs
+++ HackerRankSolvings/HackerRankSolvings/Challenges/WarmUp/RepeatedString.cs
@@ -51,7 +51,7 @@ The first n=10 letters of the infinite string are abaabaabaa. Because there are 
             Console.WriteLine();
 
             Console.WriteLine("Input a string to repeat:");
-            var s = Console.ReadLine()?.Trim();
+            var s = Console.ReadLine()?.Trim() ?? "";
 
             Console.WriteLine("\nInput the number of characters to consider:");
             long n = Convert.ToInt64(Console.ReadLine()?.Trim());
